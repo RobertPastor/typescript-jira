5064804a1c6a55967ba81fab145b3d63355cd808
Robert <robert.pastor0691@orange.fr>
2021-05-15 19:52:46 +0200

    insert history items

--- src/helpers/jiraHistory.ts
+++ src/helpers/jiraHistory.ts
@@ -1,7 +1,7 @@
 
 import { log } from './log'
-import { readHistoryItems, HistoryItemsArray } from './jiraHistoryItems'
-import { Connection } from 'tedious'
+import { readHistoryItems, HistoryItemsArray, HistoryItem } from './jiraHistoryItems'
+import { Connection, TYPES, Request } from 'tedious'
 
 export interface JiraHistory {
 
@@ -11,8 +11,9 @@ export interface JiraHistory {
     items: HistoryItemsArray
 }
 
-function readHistory(history: JiraHistory): void {
+function readHistory(history: JiraHistory): historyItemsArray {
 
+    let historyItemsArray: HistoryItemsArray = []
     log("--- read Jira history  --- ")
     for (const key in history) {
         //log(key + " - " + JSON.stringify(history[key]))
@@ -20,34 +21,99 @@ function readHistory(history: JiraHistory): void {
             log("history created -> " + history.created)
         }
         if (key == "items") {
-            readHistoryItems(history[key])
+            historyItemsArray = historyItemsArray.concat(readHistoryItems(history.created, history[key]))
         }
     }
+    return historyItemsArray
 }
 
-export function insertHistories(histories: any): void {
+export function insertHistories(histories: any): HistoryItemsArray {
 
+    let historyItemsArray: HistoryItemsArray = []
     log("====== read histories =============")
     if (Array.isArray(histories)) {
         histories.forEach(history => {
-            log("history -> " + JSON.stringify(history))
-            readHistory(history)
+            //log("history -> " + JSON.stringify(history))
+            historyItemsArray = historyItemsArray.concat(readHistory(history))
         })
     }
+    return historyItemsArray
+}
+
+function insertHistoryItem(jiraObj: any, historyItem: HistoryItem, connection: Connection): Promise<boolean> {
+
+    return new Promise(function (resolve, reject) {
+
+        let sql: string = "INSERT INTO [dbo].DEMO_ECR_PCR_HISTORY (CR, HISTORY_CREATED , ITEMS_FIELD, ITEMS_FROMSTRING, ITEMS_TOSTRING) "
+        sql += " VALUES (@CR, @HISTORY_CREATED, @ITEMS_FIELD, @ITEMS_FROMSTRING, @ITEMS_TOSTRING)"
+        let request = new Request(sql, function (err: any, rowCount: number) {
+            if (err) {
+                log(JSON.stringify(err))
+                reject(err)
+            } else {
+                log('Insert complete.');
+                resolve(true)
+            }
+        })
+
+        request.addParameter("CR", TYPES.NVarChar, jiraObj.key);
+        request.addParameter("HISTORY_CREATED", TYPES.NVarChar, historyItem.created);
+        request.addParameter("ITEMS_FIELD", TYPES.NVarChar, historyItem.field);
+        request.addParameter("ITEMS_FROMSTRING", TYPES.NVarChar, historyItem.fromString)
+        request.addParameter("ITEMS_TOSTRING", TYPES.NVarChar, historyItem.toString)
+
+        request.on('row', function (columns) {
+            columns.forEach(function (column) {
+                console.log(column.value);
+            });
+        })
+        connection.execSql(request);
+    })
 }
 
 export function insertJiraChangeLog(jiraObj: any, connection: Connection): Promise<boolean> {
 
     return new Promise(function (resolve, reject) {
 
+        let historyItemsArray: HistoryItemsArray = []
         log("----------- insert JIRA change log ---------------")
         log(JSON.stringify(jiraObj.changelog))
         for (const key in jiraObj.changelog) {
             log("========== history ======")
             //log(key + " - " + JSON.stringify(changelog[key]))
             if (key == "histories") {
-                insertHistories(jiraObj.changelog.histories)
+                historyItemsArray = historyItemsArray.concat(insertHistories(jiraObj.changelog.histories))
             }
+        }
+        //log(JSON.stringify(historyItemsArray))
+
+        if (historyItemsArray.length > 0) {
+
+            let historyItem: HistoryItem
+            let index: number = 0
+            function next(historyItem: HistoryItem) {
+                insertHistoryItem(jiraObj, historyItem, connection)
+                    .then(_ => {
+                        log("---> one history item inserted OK")
+                        index = index + 1
+                        if (index < historyItemsArray.length) {
+                            historyItem = historyItemsArray[index]
+                            next(historyItem)
+                        } else {
+                            log("---> all history items correctly inserted -> OK")
+                            resolve(true)
+                        }
+                    })
+                    .catch(err => {
+                        log(JSON.stringify(err))
+                        reject(err)
+                    })
+
+            }
+            historyItem = historyItemsArray[index]
+            next(historyItem)
+        } else {
+            resolve(true)
         }
 
     })
--- src/helpers/jiraHistoryItems.ts
+++ src/helpers/jiraHistoryItems.ts
@@ -3,6 +3,7 @@ import { log } from './log'
 
 export interface HistoryItem {
 
+    created: string,
     field: string,
     fieltype: string,
     from: any,
@@ -15,21 +16,25 @@ export interface HistoryItemsArray extends Array<HistoryItem> { }
 
 
 //{"field":"labels","fieldtype":"jira","from":null,"fromString":null,"to":null,"toString":"V5.0"}
-function readHistoryItem(historyItem: HistoryItem): void {
+function readHistoryItem(created: string, historyItem: HistoryItem): HistoryItem {
 
+    historyItem.created = created
     log("field = " + historyItem.field)
-    log("from = " + historyItem.fromString)
-    log("to = " + historyItem.toString)
+    //log("fromString = " + historyItem.fromString)
+    //log("toString = " + historyItem.toString)
+    return historyItem;
 
 }
 
-export function readHistoryItems(items: HistoryItemsArray): void {
+export function readHistoryItems(created: string, items: HistoryItemsArray): HistoryItemsArray {
+    let historyItemsArray: HistoryItemsArray = []
     log("--------- read Jira History items ------------")
     if (Array.isArray(items)) {
         items.forEach(item => {
             log("-------- Jira History item ----------")
             //log(JSON.stringify(item))
-            readHistoryItem(item)
+            historyItemsArray.push(readHistoryItem(created, item))
         })
     }
+    return historyItemsArray;
 }
